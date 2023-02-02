835fd5b2cdc5d7ef1f6177f3588ae7811de99671
quanzd <3629664683@qq.com>
2023-02-02 20:57:53 +0800

    init55

---
+++ vue-begin01/src/App1.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app"></div>
+    <h2>vue中使用swiper</h2>
+</template>
+<script>
+import SwiperDemo from './components/SwiperDemo.vue'
+export default{
+    components:{SwiperDemo},
+}
+</script>
+<style>
+.app{
+    padding-top: 50px;
+    text-align: center;
+}
+</style>
